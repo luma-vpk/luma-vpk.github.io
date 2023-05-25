467699d3ed7b073a56e11caa79927d4b11c11bfa
luma-vpk <94155195+luma-vpk@users.noreply.github.com>
2023-05-25 12:06:45 +0600

    1

--- discord.js
+++ discord.js
@@ -1,10 +1,18 @@
 async function copydiscord() {
-        try {
-            await navigator.clipboard.writeText('luma#9333');
-            ds.style.display='block';
-            setTimeout("ds.style.display='none'", 2000);
-        }
-        catch (err) {
-            console.error('Ошибка: ', err);
-        }
+  try {
+    await navigator.clipboard.writeText("luma#9333");
+    ds.style.display = "block";
+    setTimeout("ds.style.display='none'", 2000);
+  } catch (err) {
+    console.error("Ошибка: ", err);
+  }
+}
+async function copydiscord1() {
+  try {
+    await navigator.clipboard.writeText("дескорд");
+    ds.style.display = "block";
+    setTimeout("ds.style.display='none'", 2000);
+  } catch (err) {
+    console.error("Ошибка: ", err);
+  }
 }
